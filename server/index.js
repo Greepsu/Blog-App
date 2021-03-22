34910951484c75c65dd44cc9037bb0a3ebe82294
Greepsu <arnaud.mgn2@gmail.com>
2021-03-22 19:14:39 +0100

    Connect project to the database using mongoose

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 5001;
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 //Import posts routes
 const postsRoute = require('./routes/posts')
 app.use('/api', postsRoute)
+
+//Connect to Database
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log("Connected to the Database")
+)
 
 app.listen(port, () => console.log(`Blog app listening on port ${port} !`));
