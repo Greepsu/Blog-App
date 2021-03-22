658e1606196044e0d74054b58385a5addb592b15
Greepsu <arnaud.mgn2@gmail.com>
2021-03-22 20:01:44 +0100

    Create GET and POST route connected to the DB

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const router = express.Router();
+const Post = require('../models/Post')
 
-router.get('/', (req, res) => {
-    res.send('Router connected')
+
+router.get('/', async (req, res) => {
+    try {
+        const post = await Post.find()
+        res.send(post)
+        console.log(post)    
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.post('/', async (req, res) => {
+    const post = new Post({
+        author: req.body.author,
+        message: req.body.message
+    })
+    console.log(req.body)
+    try {
+        const savedPost = await post.save();
+        res.send(savedPost)
+    } catch (error) {
+        console.log(error)
+        res.status(400).send(error)
+    }
 })
 
 module.exports = router
