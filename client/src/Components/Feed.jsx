ecc36a019aff75eec3496f9d742aacdbeb4f3a9f
Greepsu <arnaud.mgn2@gmail.com>
2021-03-23 15:57:23 +0100

    Organize post-container section (center)

--- client/src/Components/Feed.jsx
+++ client/src/Components/Feed.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import '../styles/Feed.scss'
+
+//Import Components
 import Header from './Header'
 import Post from './Post'
+import UserInput from './UserInput'
 
 export default function Feed()  {
     return(
         <div className="feed">
             <div className="feed-container">
                 <Header />
-                <Post />
+                <section>
+                    <div className="posts-container">
+                        <Post />
+                        <Post />
+                        <Post />
+                        <Post />
+                    </div>
+                    <UserInput />
+                </section>
             </div>
         </div>
     )
---
+++ client/src/Components/UserInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function UserInput() {
+    return (
+        <div>
+            <input type="text" placeholder="New message..." />
+            <button>Post !</button>
+        </div>
+    )
+}
