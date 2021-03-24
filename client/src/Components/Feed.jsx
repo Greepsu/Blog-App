ca47429734e21d2ba6020a962a87ace8d1a00bd4
Greepsu <arnaud.mgn2@gmail.com>
2021-03-24 12:18:12 +0100

    Create transition between component

--- client/src/Components/Feed.jsx
+++ client/src/Components/Feed.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
-import '../styles/Feed.scss'
+import React from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { motion } from "framer-motion";
+
+import "../styles/Feed.scss";
 
 //Import Components
-import Header from './Header'
-import Post from './Post'
-import UserInput from './UserInput'
+import Header from "./Header";
+import Post from "./Post";
+import UserInput from "./UserInput";
 
-export default function Feed()  {
-    return(
-        <div className="feed">
-            <div className="feed-container">
-                <Header pageName="Feed" />
-                <section>
-                    <UserInput />
-                    <div className="posts-container">
-                        <Post />
-                        <Post />
-                        <Post />
-                        <Post />
-                    </div>
-                </section>
-            </div>
-            <Link className="arrow-right" to="/private-message"></Link>
-        </div>
-    )
+export default function Feed() {
+  return (
+    <motion.div
+      initial={{ x: "-40%" }}
+      animate={{ x: 0 }}
+      transition={{ ease: "easeOut", duration: 0.4 }}
+      className="feed"
+    >
+      <div className="feed-container">
+        <Header pageName="Feed" />
+        <section>
+          <UserInput />
+          <div className="posts-container">
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+          </div>
+        </section>
+      </div>
+        <Link to="/private-message">
+            <motion.div whileHover={{ scale: 1.2 }} className="arrow-right"></motion.div>
+        </Link>
+    </motion.div>
+  );
 }
--- client/src/Components/PrivateMessage.jsx
+++ client/src/Components/PrivateMessage.jsx
@@ -1,27 +1,37 @@
 import React from "react";
-import '../styles/PrivateMessage.scss'
+import "../styles/PrivateMessage.scss";
 
+//import Components
 import Chat from "./Chat";
 import Header from "./Header";
 
 //import users images
-import kerryPicture from '../assets/users-image/photo-kerry-hebert.jpg'
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import kerryPicture from "../assets/users-image/photo-kerry-hebert.jpg";
 
+//import Lib
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { motion } from "framer-motion";
 
 export default function PrivateMessage() {
   return (
-    <div className="private-message" >
+    <motion.div
+      initial={{ x: "40%" }}
+      animate={{ x: 0 }}
+      transition={{ ease: "easeOut", duration: 0.4 }}
+      className="private-message"
+    >
       <Header pageName="Private message" />
-      <Link className="arrow-left" to="/"></Link>
+      <Link to="/">
+      <motion.div whileHover={{ scale: 1.2 }} className="arrow-left"></motion.div>
+      </Link>
       <section>
         <div className="bubble">
-          <img src={kerryPicture} alt="user"/>
+          <img src={kerryPicture} alt="user" />
           <span className="notif">1</span>
         </div>
-          <div className="separator"></div>
-          <Chat />
+        <div className="separator"></div>
+        <Chat />
       </section>
-    </div>
+    </motion.div>
   );
 }
