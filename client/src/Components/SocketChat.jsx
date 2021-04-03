cb840be5f2a6f5271c73b8117743c2c4e72a3711
Greepsu <arnaud.mgn2@gmail.com>
2021-04-03 17:59:51 +0200

    Implement Delete button

--- client/src/Components/SocketChat.jsx
+++ client/src/Components/SocketChat.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 //import Lib
 import { io } from "socket.io-client";
 
-
 const SocketChat = () => {
   const socket = io('http://localhost:5001');
   socket.on("connect", () => {
